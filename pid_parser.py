2995971e01f3547f1f09c65599bca7c5ac2a6d05
parkm <minjoo.park@technicolor.com>
2021-07-29 13:46:34 +0900

    Parse header and write to .txt file

--- pid_parser.py
+++ pid_parser.py
@@ -1,12 +1,15 @@
 import glob
 import os
 
+PACKET_NUMBER = 0
+
 
 def open_file(file_input):
     # while True:
     #   ts_packets = []
     # testing_array = []
     #     num = 0
+    # Ask for file name to user
     #     # file_input = input("Enter the filename of .ts: ")
     #     # if os.path.exists(file_input + '.ts'):
     # file_open = open(file_input + '.ts', "rb")
@@ -20,7 +23,6 @@ def open_file(file_input):
 
         if len(reading_file) < 188:
             break
-
         decompose_file(reading_file)
 
 
@@ -29,6 +31,7 @@ def decompose_file(one_packet_bytes):
     sync_byte = one_packet_bytes[0]                                         # Sync byte (8)
     continuity_counter_list = []
     num = 0
+    global PACKET_NUMBER
     # user_pid = enter_pid()
 
     if sync_byte != SYNC_BYTE:
@@ -41,35 +44,59 @@ def decompose_file(one_packet_bytes):
     transport_priority = (one_packet_bytes[1] >> 5) & 1                     # transport_priority (1)
     pid_left = one_packet_bytes[1] & 0x1F                                   # pid (13)
     pid_right = one_packet_bytes[2] & 0xFF
-    pid = pid_left << 8 + pid_right
+    pid = (pid_left << 8) + pid_right
     transport_scrambling_control = (one_packet_bytes[3] & 0xC0) >> 6        # transport_scrambling_control (2)
     adaptation_field_control = (one_packet_bytes[3] & 0x30) >> 4            # adaptation_field_control (2)
     continuity_counter = one_packet_bytes[3] & 0x0F                         # continuity_counter (4)
 
-    # print("Sync Byte                       ", hex(sync_byte))
-    # print("Transport Error Indicator       ", bin(transport_error_indicator))
-    # print("Transport Priority              ", bin(transport_priority))
-    # print("Payload Unit Start Indicator    ", bin(payload_unit_start_indicator))
-    # print("Transport Priority              ", transport_priority)
-    # print("PID                             ", f'0x{pid_left:x}{pid_right:x}')
-    # print("Transport Scrambling Control    ", bin(transport_scrambling_control))
-    # print("Adaptation Field Control        ", bin(adaptation_field_control))
-    # print("Continuity Counter              ", bin(continuity_counter), "(", hex(continuity_counter), ")")
-    # print("\n")
+    print("Header: ", end='')
+    file_output.write("Header: ")
+    for i in range(0, 4):
+        print('{0:0{1}X}'.format(one_packet_bytes[i], 2), end=' ')
+        file_output.write("%s " % '{0:0{1}X}'.format(one_packet_bytes[i], 2))
 
+    # print("\n")
+    print("\n     Packet Number:                  ", PACKET_NUMBER)
+    file_output.write("\n\t Packet Number: \t\t\t %d" % PACKET_NUMBER)
+
+    print("     Sync Byte                       ", hex(sync_byte))
+    file_output.write("\n\t Sync Byte \t\t\t %s" % hex(sync_byte))
+
+    print("     Transport Error Indicator       ", bin(transport_error_indicator))
+    file_output.write("\n\t Transport Error Indicator \t\t %s" % bin(transport_error_indicator))
+
+    print("     Payload Unit Start Indicator    ", bin(payload_unit_start_indicator))
+    file_output.write("\n\t Payload Unit Start Indicator \t\t %s" % bin(payload_unit_start_indicator))
+
+    print("     Transport Priority              ", bin(transport_priority))
+    file_output.write("\n\t Transport Priority \t\t\t %s" % bin(transport_priority))
+
+    print("     PID                             ", '0x{0:0{1}X}'.format(pid, 4))
+    file_output.write("\n\t PID    \t\t\t\t %s" % '0x{0:0{1}X}'.format(pid, 4))
+
+    print("     Transport Scrambling Control    ", bin(transport_scrambling_control))
+    file_output.write("\n\t Transport Scrambling Control \t %s" % bin(transport_scrambling_control))
+
+    print("     Adaptation Field Control        ", bin(adaptation_field_control))
+    file_output.write("\n\t Adaptation Field Control \t\t %s" % bin(adaptation_field_control))
+
+    print("     Continuity Counter              ", bin(continuity_counter), "(", hex(continuity_counter), ")")
+    file_output.write("\n\t Continuity Counter \t\t %s (%s)" % (bin(continuity_counter),  hex(continuity_counter)))
+
+    print("\n")
+    file_output.write("\n\n")
+
+    PACKET_NUMBER = PACKET_NUMBER + 1
     # if user_pid == (f'0x{pid_left:x}{pid_right:x}'):
     #     print("You have entered", user_pid, ".")
 
     # Checking conditions for PAT
-    if pid_left == 0x0 and pid_right == 0x0 and payload_unit_start_indicator == 0b1:
-        print(">> CONDITIONS MET FOR PID AND PUSI. PAT = TRUE")
-        calculate_PAT(one_packet_bytes)
+    # if pid_left == 0x0 and pid_right == 0x0 and payload_unit_start_indicator == 0b1:
+    #     print(">> CONDITIONS MET FOR PID AND PUSI. PAT = TRUE")
+    #     calculate_PAT(one_packet_bytes)
 
-    # if pid_left == 0x0 and pid_right == 0x0:
     # if pid == 0x0:
         # print("pid", hex(pid))
-        # print("pid_left", pid_left)
-        # print("pid_right", pid_right)
         # print("Continuity Counter              ", bin(continuity_counter), "(", hex(continuity_counter), ")")
 
     for b in continuity_counter_list:
@@ -82,15 +109,16 @@ def decompose_file(one_packet_bytes):
     #     print(">> Third condition met for PAT")
 
     # Four bytes after 0x47
-    # print("---------- Four Bytes ----------")
-    # for i in range(0, 5):
+    #     # print("---------- Four Bytes ----------")
+    #     # for i in range(0, 5):
         # print("ByteArray", bytearray(one_packet_bytes))
-        # print("one_packet_bytes {}".format(i), ": ",  one_packet_bytes[i], ". In binary: ", '{0:08b}'.format(one_packet_bytes[i]),
+        # print("one_packet_bytes {}".format(i), ": ",  one_packet_bytes[i], ". In binary: ",
+        #  '{0:08b}'.format(one_packet_bytes[i]),
         #       "(", bin(one_packet_bytes[i]), ")")
 
     # <<< PAT INFORMATION >>>
     # if one_packet_bytes[5] == 0x0:
-    #     print("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    #     print("YEEEEEEEEEEEEEEEEEEEEE")
     #     calculate_PAT(one_packet_bytes)
 
     # # if os.path.exists(file_input):
@@ -160,48 +188,38 @@ def calculate_PAT(one_packet_bytes):
     print("---------- PAT Information ----------")
 
     table_id = one_packet_bytes[5]                  # table_id (8)
-
-    print("table_id: ", hex(table_id))
-
     section_syntax_indicator = (one_packet_bytes[6] >> 7) & 1
     reserved_future_use = (one_packet_bytes[6] >> 6) & 1
     reserved = (one_packet_bytes[3] & 0x30) >> 4
     section_length_left = one_packet_bytes[6] & 0x0F  # <simplify if possible> section length (12)
-    section_length_right = one_packet_bytes[7] & 0xFF
-    section_length = section_length_left << 8 + section_length_right
-
-    print("section_syntax_indicator2", bin(section_syntax_indicator))
-    print("reserved_future_use", bin(reserved_future_use))
-    print("reserved", bin(reserved))
-    print("section length test", (f'0x{section_length_left:x}{section_length_right:x}'))
-    # print("section_length", hex(section_length))
-
-    transport_stream_id = one_packet_bytes[8] + one_packet_bytes[9]
-    print("transport_stream_id", hex(transport_stream_id))
-    print("transprot_stream_id8", hex(one_packet_bytes[8]))
-    print("transprot_stream_id9", hex(one_packet_bytes[9]))
-
+    section_length_right = one_packet_bytes[7] & 0xFF3
+    section_length = (section_length_left << 8) + section_length_right
+    transport_stream_id = (one_packet_bytes[8] << 8) | one_packet_bytes[9]
     reserved_two = (one_packet_bytes[10] & 0xC0) >> 6
-    print("reserved_#2", bin(reserved_two))
 
-    version_number = (one_packet_bytes[10] & 0x3F) >> 4
+    current_next_indicator = one_packet_bytes[10] & 1
+    section_number = one_packet_bytes[11]  # section number (8)
+    last_section_number = one_packet_bytes[12]  # last section number (8)
+    program_number = (one_packet_bytes[13] << 8) + one_packet_bytes[14]
+
+    print("Table ID:                    ", hex(table_id))
+
+    print("Section Syntax Indicator:    ", bin(section_syntax_indicator))
+    print("Reserved Future Use:         ", bin(reserved_future_use))
+    print("Reserved:                    ", bin(reserved))
+    print("Section Length Test:         ", f'0x{section_length_left:x}{section_length_right:x}')
+    # print("section_length             ", hex(section_length))
+    print("Transport Stream ID:         ", hex(transport_stream_id))
+    print("Reserved #2:                 ", bin(reserved_two))
+
+    version_number = (one_packet_bytes[10] & 0x3E) >> 5
+    print("one_packet[10]", bin(one_packet_bytes[10]))
     print("version number", bin(version_number))
 
-    current_next_indicator = (one_packet_bytes[10] >> 1) & 1
-    print("current_next_indicator", bin(current_next_indicator))
-
-    section_number = one_packet_bytes[11] & 0xFF             # section number (8)
-    print("section_number", hex(section_number))
-
-    last_section_number = one_packet_bytes[12] & 0xFF          # last section number (8)
-    print("last_section_number", hex(last_section_number))
-
-    program_number1 = one_packet_bytes[13]              # program number (16)
-    program_number2 = one_packet_bytes[14]
-    program_number = program_number1 << 8 + program_number2
-
-    print("program_number", f'0x{program_number1:x}{program_number2:x}')
-    print("Program number2", program_number)
+    print("Current Next Indicator:      ", bin(current_next_indicator))
+    print("Section Number:              ", hex(section_number))
+    print("Last Section Number:         ", hex(last_section_number))
+    print("Program Number:              ", hex(program_number))
 
 
 def calculate_PMT():
@@ -212,10 +230,16 @@ def calculate_PMT():
 def main():
     open_file(file_input)       # In future, delete file_input to choosing file by asking the user or from file explorer
 
+    file_output.close()
+
 
 if __name__ == "__main__":
     PACKET_SIZE = 188
     SYNC_BYTE = 0x47
 
-    file_input = "C:\\Users\\parkm\\Desktop\\dump_690000_25.ts"
+    # file_input = "C:\\Users\\parkm\\Desktop\\dump_690000_25.ts"
+    file_input = "C:\\Users\\parkm\\Desktop\\France 20080505_Ch1.ts"
+    file_output = open("C:\\Users\\parkm\\Desktop\\ts_parser_python.txt", "w")
+
     main()
+
